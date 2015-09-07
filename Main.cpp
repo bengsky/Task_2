df365a52f2e6d30da08802802a3b2fdc2f289cc7
bengsky <iqbal1bahari@gmail.com>
2015-09-07 23:06:46 +0700

    Update Main.cpp
    task2 1103134314

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include "clock.h"
 
 using namespace std;
 
 int main()
 {
+    clock c1, c2, c3;
+    cout << "Hello world!" << endl;
+    c1= MakeClock(2,30,4);
+    c2= MakeClock(6,0,0);
+    c3= MakeClock(1,62,30);
+
+    PrintClock(c1);
+    PrintClock(c2);
+
+    cout<<GetHour(c1);
+    cout<<GetSecond(c2);
+
+    c3=AddClock(c1,c2);
+    PrintClock(c3);
+    cout<<IsEqual(c1,c2);
+
   return 0;
 }
